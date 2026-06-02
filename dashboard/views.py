10db98aeb7b92ac1aa9ad4fb42c6d65e3b53a8e8
ASTHA <asthashiyani113@gmail.com>
2026-06-02 19:35:01 +0530

    Initial commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,30 @@ def get_states(request):
     )
     return JsonResponse(states, safe=False)
 
+def get_cities(request):
+    state = request.GET.get("state")
+
+    cities = list(
+        FarmData.objects.filter(
+            state=state
+        ).values_list(
+            "city",
+            flat=True
+        ).distinct()
+    )
+
+    return JsonResponse(cities, safe=False)
+
+def get_crops(request):
+    state = request.GET.get("state")
+
+    crops = list(
+        FarmData.objects.filter(
+            state=state
+        ).values_list(
+            "crop_type",
+            flat=True
+        ).distinct()
+    )
+
+    return JsonResponse(crops, safe=False)
